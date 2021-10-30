a28f9ad20ff3d44f072b28f984743e1a0070f7de
rosh-1228 <sho.watanabe1228@gmail.com>
2021-10-31 00:48:44 +0900

    メモの新規登録・タイトル内容の変更までを実装

--- app.rb
+++ app.rb
@@ -2,10 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-get '/' do
-  File.open("json/memo.json") do |memo_title|
-    @memo_memo = JSON.load(memo_title)
+class Memo
+  def initialize
+    @memo_info = File.open("json/memodb.json") do |memo|
+      JSON.load(memo)
+    end
   end
+
+  def io_memodb
+    @memo_info
+  end
+
+end
+
+get '/' do
+  @memo_info = Memo.new.io_memodb
   erb :top
 end
 
@@ -14,39 +25,47 @@ get '/new_memo' do
 end
 
 post '/adds' do
-  json_data = open("json/memo.json") do |io|
-    JSON.load(io)
-  end
+  memo_info = Memo.new.io_memodb
 
-  if json_data.nil?
-    json_data = {"memos" => [params.merge!("id" => "1")]}
+  if memo_info.nil?
+    memo_info = {"memos" => [params.merge!("id" => "1")]}
   else
-    params.merge!("id" => json_data["memos"][-1]["id"].to_i + 1)
-    json_data["memos"] << params
+    params.merge!("id" => memo_info["memos"][-1]["id"].to_i + 1)
+    memo_info["memos"] << params
   end
 
-  File.open("json/memo.json", "w") do |file|
-    JSON.dump(json_data, file)
+  File.open("json/memodb.json", "w") do |memodb|
+    JSON.dump(memo_info, memodb)
   end
   redirect '/'
 end
 
-get '/:id/memo_list' do
-  @m_title = open("json/memo.json") do |data|
-    JSON.load(io)
-  end
-  puts json_data
-  erb :memo_list
+get '/memo/:id' do
+  @memo_info = Memo.new.io_memodb
+  erb :memo_contexts
 end
 
-get '/id' do
-  File.open("json/memo.json") do |memo_title|
-    @memo_memo = JSON.load(memo_title)
-  end
-  puts @memo_memo["memos"]
+get '/memo/:id/context' do
+  memo_info = Memo.new.io_memodb["memos"][params[:id].to_i - 1]
+  @memo_id = memo_info["id"]
+  @memo_title = memo_info["title"]
+  @memo_context = memo_info["text"]
+  erb :memo_contexts_edit
 end
 
-patch '/id' do
-  @edit_title
-  @edit_text
+patch '/memo/:id/context' do
+=begin
+  memo_info = Memo.new.io_memodb["memos"][params[:id].to_i - 1]
+  ["title"] = params[:title]
+  memo_info["text"] = params[:text]
+  update_memo = Memo.new.io_memodb["memos"][params[:id].to_i - 1].merge!(memo_info)
+  puts update_memo
+=end
+  memo_info = Memo.new.io_memodb
+  memo_info["memos"][params[:id].to_i - 1]["title"] = params[:title]
+  memo_info["memos"][params[:id].to_i - 1]["text"] = params[:text]
+  File.open("json/memodb.json", "w") do |memodb|
+    JSON.dump(memo_info, memodb)
+  end
+  redirect "/"
 end
