c3a0976c61d906ee4a253e48375d5cad9fbab757
rosh-1228 <sho.watanabe1228@gmail.com>
2021-11-03 17:32:46 +0900

    rubyメソッドを作成し、GET/POST/PATCH/DELETEメソッド内を簡略化

--- app.rb
+++ app.rb
@@ -2,20 +2,40 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 
-class Memo
-  def initialize
-    @memo_info = File.open("json/memodb.json") do |memo|
-      JSON.load(memo)
-    end
-  end
-
-  def io_memodb
-    @memo_info
+def import_json
+  File.open("json/memodb.json") do |memo|
+    JSON.load(memo)
   end
 end
 
+def take_number_from_array(id)
+  memo_hash = JSON.parse(import_json.to_json, {symbolize_names: true})
+  memo_elm = memo_hash[:memos].find {|data| data[:id].to_i == id.to_i }
+  number = memo_hash[:memos].index(memo_elm)
+end
+
+def take_memo_from_hash(id)
+  import_json["memos"][take_number_from_array(id)]
+end
+
+def update_hash(memo_params)
+  memo_params.delete("_method")
+  export_json = import_json
+  array_number = take_number_from_array(memo_params["id"])
+  memo_params = memo_params.to_h
+  export_json["memos"][array_number].replace(memo_params)
+  export_json
+end
+
+def export_json(memo_params)
+  File.open("json/memodb.json", "w") do |memodb|
+    JSON.dump(memo_params, memodb)
+  end
+end
+
+
 get '/' do
-  @memo_info = Memo.new.io_memodb
+  @memo_info = import_json
   erb :top
 end
 
@@ -24,7 +44,7 @@ get '/new_memo' do
 end
 
 post '/adds' do
-  memo_info = Memo.new.io_memodb
+  memo_info = import_json
 
   if memo_info.nil?
     memo_info = {"memos": [params.merge!("id": 1)]}
@@ -33,34 +53,25 @@ post '/adds' do
     memo_info["memos"] << params
   end
 
-  File.open("json/memodb.json", "w") do |memodb|
-    JSON.dump(memo_info, memodb)
-  end
+  export_json(memo_info)
+
   redirect '/'
 end
 
 get '/memo/:id' do
-  memo_memo = Memo.new.io_memodb
 
-  memo_info = JSON.parse(memo_memo.to_json, {symbolize_names: true})
-  memo_elm = memo_info[:memos].find {|data| data[:id].to_i == params[:id].to_i }
+  memo = take_memo_from_hash(params["id"])
 
-  number = memo_info[:memos].index(memo_elm)
-  memo = memo_memo["memos"][number]
   @memo_id = memo["id"]
   @memo_title = memo["title"]
   @memo_context = memo["text"]
+
   erb :memo_contexts
 end
 
 get '/memo/:id/context' do
-  memo_memo = Memo.new.io_memodb
+  memo = take_memo_from_hash(params["id"])
 
-  memo_info = JSON.parse(memo_memo.to_json, {symbolize_names: true})
-  memo_elm = memo_info[:memos].find {|data| data[:id].to_i == params[:id].to_i }
-
-  number = memo_info[:memos].index(memo_elm)
-  memo = memo_memo["memos"][number]
   @memo_id = memo["id"]
   @memo_title = memo["title"]
   @memo_context = memo["text"]
@@ -68,30 +79,22 @@ get '/memo/:id/context' do
 end
 
 patch '/memo/:id/context' do
-  memo_memo = Memo.new.io_memodb
-
-  memo_info = JSON.parse(memo_memo.to_json, {symbolize_names: true})
-  memo_elm = memo_info[:memos].find {|data| data[:id].to_i == params[:id].to_i }
-
-  number = memo_info[:memos].index(memo_elm)
-
-  memo_memo["memos"][number]["title"] = params["title"]
-  memo_memo["memos"][number]["text"] = params["text"]
+  
+  export_json(update_hash(params))
+=begin
+  import_json["memos"][take_number_from_array(params[:id])]["title"] = params["title"]
+  import_json["memos"][take_number_from_array(params[:id])]["text"] = params["text"]
 
   File.open("json/memodb.json", "w") do |memodb|
-    JSON.dump(memo_memo, memodb)
+    JSON.dump(import_json, memodb)
   end
-
+=end
   redirect "/"
 end
 
 delete '/memo/:id' do
-  memo_memo = Memo.new.io_memodb
-  memo_info = JSON.parse(memo_memo.to_json, {symbolize_names: true})
-  memo_elm = memo_info[:memos].find {|data| data[:id].to_i == params[:id].to_i }
 
-  number = memo_info[:memos].index(memo_elm)
-  memo_memo["memos"].delete_at(number)
+  memo_memo["memos"].delete_at(take_number_from_array(params[:id]))
 
   File.open("json/memodb.json", "w") do |memodb|
     JSON.dump(memo_memo, memodb)
