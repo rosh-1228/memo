1761c30a1a1c3dc83c3c08373a6d9a6899867b5a
rosh-1228 <sho.watanabe1228@gmail.com>
2021-11-10 16:33:40 +0900

    インスタンスを全てhelperメソッド内に移動しました

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# memo
-made memo with sinatra
+# メモ帳アプリ
+フィヨルドブートキャンプのプラクティスにて作成した提出物です。
+以下の機能を備えています。
+- メモの作成
+- メモの編集
+- メモの削除
+## システム要件
+ruby 3.0.0
+
+## インストール方法
+- Bundlerのインストール
+```
+gem install bundler
+```
+
+- Gemfileの作成
+```
+gem 'sinatra'
+gem 'sinatra-reloader'
+gem 'webrick'
+gem 'rubocop'
+gem 'json'
+```
--- app.rb
+++ app.rb
@@ -5,48 +5,48 @@ require 'sinatra/reloader'
 require 'json'
 
 helpers do
-  def h(text)
-    Rack::Utils.escape_html(text)
+  def h(param)
+    Rack::Utils.escape_html(param)
   end
-end
 
-def import_json
-  File.open('json/memodb.json') { |memo| JSON.parse(memo.read) }
-end
-
-memos = import_json
-
-def fetch_memo(memos, memo_param)
-  memos['memos'].find_index { |data| data['id'].to_i == memo_param['id'].to_i }
-end
-
-def update_memo(memos, memo_params)
-  memo_params.delete('_method')
-  memos['memos'][fetch_memo(memos, memo_params)].replace(memo_params)
-  memos
-end
-
-def export_json(memo_params)
-  File.open('json/memodb.json', 'w') { |memodb| JSON.dump(memo_params, memodb) }
-end
-
-def memos?(memos, params)
-  if memos.nil? || memos['memos'][0].nil?
-    memos = { 'memos': [params.merge!('id': 1)] }
-  else
-    memos['memos'] << params.merge!('id': memos['memos'][-1]['id'].to_i + 1)
+  def import_json
+    @memos = JSON.load(File.open('json/memodb.json').read)
   end
-  memos
-end
 
-def update_memos(memos, params)
-  params.transform_values! { |params_value| h(params_value) }
-  export_json(memos?(memos, params))
+  def fetch_memo_number(memos, memo_param)
+    memos['memos'].find_index { |data| data['id'].to_i == memo_param['id'].to_i }
+  end
+
+  def fetch_memo(memos, params)
+    @memo = memos['memos'][fetch_memo_number(memos, params)]
+  end
+
+  def export_json(params)
+    params['memos'].each do |param|
+      param.transform_keys! {|key| h(key)}
+      param.transform_values! {|key| h(key)}
+    end
+    File.open('json/memodb.json', 'w') { |memodb| JSON.dump(params, memodb) }
+  end
+  
+  def memos?(memos, params)
+    if memos.nil? || memos['memos'][0].nil?
+      memos = { 'memos'=>[params.merge!('id': 1)] }
+    else
+      memos['memos'] << params.merge!('id': memos['memos'][-1]['id'].to_i + 1)
+    end
+    memos
+  end
+
+  def update_memo(memos, memo_params)
+    memo_params.delete('_method')
+    memos['memos'][fetch_memo_number(memos, memo_params)].replace(memo_params)
+    export_json(memos)
+  end
 end
 
 get '/' do
-  memos = import_json
-  @memo_info = memos
+  import_json
   erb :top
 end
 
@@ -54,28 +54,29 @@ get '/new_memo' do
   erb :new_memo
 end
 
-post '/adds' do
-  update_memos(memos, params)
+post '/new_memo' do
+  export_json(memos?(import_json, params))
   redirect '/'
 end
 
 get '/memo/:id' do
-  @memo = memos['memos'][fetch_memo(memos, params)]
+  fetch_memo(import_json, params)
   erb :memo_contexts
 end
 
 get '/memo/:id/context' do
-  @memo = memos['memos'][fetch_memo(memos, params)]
+  fetch_memo(import_json, params)
   erb :memo_contexts_edit
 end
 
 patch '/memo/:id/context' do
-  update_memos(memos, params)
+  update_memo(import_json, params)
   redirect '/'
 end
 
 delete '/memo/:id' do
-  memos['memos'].delete_at(fetch_memo(memos, params))
+  memos = import_json
+  memos['memos'].delete_at(fetch_memo_number(memos, params))
   export_json(memos)
   redirect '/'
 end
