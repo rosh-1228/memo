15c474aaf612b02f79dd45f7939314d70a6a5afa
rosh-1228 <sho.watanabe1228@gmail.com>
2021-11-15 12:22:23 +0900

    import_jsonメソッドにて、FileTest.zero?を用いて、ファイルサイズが０かどうかを確認する処理に変更

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -6,12 +6,41 @@ helpers do
   end
 
   def import_json
+=begin
     memo_file = File.open('json/memodb.json').read
+    p memo_file
     @memos = JSON.parse(memo_file) if memo_file != ''
+=end
+    @memos = FileTest.zero?('json/memodb.json')? nil : JSON.parse(File.open('json/memodb.json').read)
   end
 
   def fetch_memo(memos, params)
     @memo = memos['memos'][fetch_memo_number(memos, params)]
+  end
+
+  def fetch_memo_number(memos, memo_param)
+    memos['memos'].find_index { |data| data['id'].to_i == memo_param['id'].to_i }
+  end
+
+  def memos?(memos, params)
+    params.transform_values! { |key| h(key) }
+    if memos.nil? || memos['memos'][0].nil?
+      memos = { 'memos' => [params.merge!('id': 1)] }
+    else
+      memos['memos'] << params.merge!('id': memos['memos'][-1]['id'].to_i + 1)
+    end
+    memos
+  end
+
+  def export_json(params)
+    File.open('json/memodb.json', 'w') { |memodb| JSON.dump(params, memodb) }
+  end
+
+  def update_memo(memos, params)
+    params.delete('_method')
+    params.transform_values! { |key| h(key) }
+    memos['memos'][fetch_memo_number(memos, params)].replace(params)
+    export_json(memos)
   end
 end
 
